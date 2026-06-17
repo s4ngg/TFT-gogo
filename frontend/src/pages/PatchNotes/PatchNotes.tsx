90540856839068f06fd058ebec1e01482513dfb9
TIG-korea <leehjpp4@gmail.com>
2026-06-17 17:00:18 +0900

    refactor: reduce patch note change repetition

--- frontend/src/pages/PatchNotes/PatchNotes.tsx
+++ frontend/src/pages/PatchNotes/PatchNotes.tsx
@@ -102,6 +102,7 @@ function PatchNotes() {
             />
 
             <PatchChangeList
+              activeChangeType={activeChangeType}
               patchChanges={patchChanges}
             />
 
